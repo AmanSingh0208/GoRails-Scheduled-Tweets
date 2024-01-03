5f676aaa25239abb48a58d3bb73475dbfc887bfa
AmanSingh0208 <amansingh020899@gmail.com>
2024-01-03 19:07:49 +0530

    Add flash messages

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,5 +1,7 @@
 class MainController < ApplicationController
   def index
-
+    # flash.now[:notice] ="Logged in successfully.."
+    # flash.now[:alert] ="Details not matched !!"
+    # flash.now[:reminder] ="Logout before leaving.."
   end
 end
